a9c0e5d1f0616c80eac885d1e63c351b591e46bd
Matheus Araujo <matheus.saraujo@gmail.com>
2019-08-27 00:57:23 -0300

    eslint

--- src/App.js
+++ src/App.js
@@ -3,21 +3,20 @@ import Graph from 'react-graph-vis';
 import './App.css';
 
 function App() {
-
   const graph = {
     nodes: [
-      { id: 1, label: "Node 1", title: "node 1 tootip text" },
-      { id: 2, label: "Node 2", title: "node 2 tootip text" },
-      { id: 3, label: "Node 3", title: "node 3 tootip text" },
-      { id: 4, label: "Node 4", title: "node 4 tootip text" },
-      { id: 5, label: "Node 5", title: "node 5 tootip text" }
+      { id: 1, label: 'Node 1', title: 'node 1 tootip text' },
+      { id: 2, label: 'Node 2', title: 'node 2 tootip text' },
+      { id: 3, label: 'Node 3', title: 'node 3 tootip text' },
+      { id: 4, label: 'Node 4', title: 'node 4 tootip text' },
+      { id: 5, label: 'Node 5', title: 'node 5 tootip text' },
     ],
     edges: [
       { from: 1, to: 2 },
       { from: 1, to: 3 },
       { from: 2, to: 4 },
-      { from: 2, to: 5 }
-    ]
+      { from: 2, to: 5 },
+    ],
   };
 
   const options = {};
@@ -26,9 +25,6 @@ function App() {
     <Graph
       graph={graph}
       options={options}
-      getNetwork={network => {
-        //  if you want access to vis.js network api you can set the state in a parent component using this property
-      }}
     />
   );
 }
