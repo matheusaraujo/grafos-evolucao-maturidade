77017e4930ffb71c3b7c65f04098562f966643c3
Matheus Araujo <matheus.saraujo@gmail.com>
2019-08-27 01:27:25 -0300

    eslint

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       { id: 2, label: "Node 2", title: 'node 2 tootip text' },
       { id: 3, label: "Node 3", title: 'node 3 tootip text' },
       { id: 4, label: "Node 4", title: 'node 4 tootip text' },
-      { id: 5, label: 'Node 5', title: 'node 5 tootip text' },
+      { id: 5, label: "Node 5", title: 'node 5 tootip text' },
     ],
     edges: [
       { from: 1, to: 2 },
