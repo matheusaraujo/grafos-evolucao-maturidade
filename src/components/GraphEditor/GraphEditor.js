cf90dbbb79b2e9da2f8c740ac0044b16f5bec060
Matheus Araujo <matheus.saraujo@gmail.com>
2019-09-17 23:17:05 -0300

    modal editar vertice

--- src/components/GraphEditor/GraphEditor.js
+++ src/components/GraphEditor/GraphEditor.js
@@ -115,7 +115,7 @@ const GraphEditor = ({
       <div>
         {selectedTab === 'graph' && graphAceEditor}
         {selectedTab === 'options' && aceEditorOptions}
-      </div>      
+      </div>
       <NodeEditor />
     </div>
   );
--- src/reducers/page.test.js
+++ src/reducers/page.test.js
@@ -16,6 +16,7 @@ describe('page reducer', () => {
       .toEqual({
         lang: 'pt',
         loaded: false,
+        nodeEdition: false,
       });
   });
 });
