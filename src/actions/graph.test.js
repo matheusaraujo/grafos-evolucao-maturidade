ed98c719d469eec8ae2e2dec92fe4a021c77fb61
Matheus Araujo <matheus.saraujo@gmail.com>
2019-09-26 13:52:16 -0300

    test coverage

--- src/actions/graph.test.js
+++ src/actions/graph.test.js
@@ -5,4 +5,12 @@ describe('actions graph', () => {
     expect(actions.updateGraph({ edges: [], nodes: [] }))
       .toEqual({ type: 'UPDATE_GRAPH', graph: { edges: [], nodes: [] } });
   });
+  test('updateNodes should create UPDADE_NODES action', () => {
+    expect(actions.updateNodes([]))
+      .toEqual({ type: 'UPDATE_NODES', nodes: [] });
+  });
+  test('updateEdges should create UPDADE_EDGES action', () => {
+    expect(actions.updateEdges([]))
+      .toEqual({ type: 'UPDATE_EDGES', edges: [] });
+  });
 });
--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -5,21 +5,22 @@ describe('actions index', () => {
     expect(actions.updateOptions({ layout: { hierarchical: { enabled: true, direction: 'LR' } } }))
       .toEqual({ type: 'UPDATE_OPTIONS', options: { layout: { hierarchical: { enabled: true, direction: 'LR' } } } });
   });
-
+  test('updateNodeGroups should create UPDATE_NODE_GROUPS action', () => {
+    expect(actions.updateNodeGroups([]))
+      .toEqual({ type: 'UPDATE_NODE_GROUPS', nodeGroups: [] });
+  });
   test('showModal should create SHOW_MODAL action', () => {
     expect(actions.showModal())
       .toEqual({
         type: 'SHOW_MODAL',
       });
   });
-
   test('hideModal should create HIDE_MODAL action', () => {
     expect(actions.hideModal())
       .toEqual({
         type: 'HIDE_MODAL',
       });
   });
-
   test('fillModal should create FILL_MODAL action', () => {
     expect(actions.fillModal('blah', undefined, 'foo'))
       .toEqual({
