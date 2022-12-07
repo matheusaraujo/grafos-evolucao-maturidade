e09c23656c4d1ff4ca9f3d17b7d291c7677fc0fd
Matheus Araujo <matheus.araujo@dazn.com>
2022-12-07 15:31:54 +0100

    Updates

---
+++ precache-manifest.9ade088e42729a0df8c506447aacf142.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eaf649b3fe84c1da5ba94fb1d3d3c57",
+    "url": "/grafos-evolucao-maturidade/index.html"
+  },
+  {
+    "revision": "9b04d55bc4ef918c3508",
+    "url": "/grafos-evolucao-maturidade/static/css/main.0197050c.chunk.css"
+  },
+  {
+    "revision": "1be42ea9bf3d31044581",
+    "url": "/grafos-evolucao-maturidade/static/js/2.2999ebe3.chunk.js"
+  },
+  {
+    "revision": "9b04d55bc4ef918c3508",
+    "url": "/grafos-evolucao-maturidade/static/js/main.cdb681cf.chunk.js"
+  },
+  {
+    "revision": "eb0e6573b2fd854341b5",
+    "url": "/grafos-evolucao-maturidade/static/js/runtime~main.1838f90b.js"
+  }
+]);
